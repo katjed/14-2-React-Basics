c3cb7b96c25c62bb9164fad5455b8bc36efd0401
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-02-19 18:02:29 +0100

    Update files

--- js/script.js
+++ js/script.js
@@ -12,47 +12,47 @@ var element =
 				React.createElement('p', {}, 'Film o czarodzieju')
 			),
 			React.createElement('li', {},
-        		React.createElement('h2', {}, 'Król Lew'),
-        		React.createElement('p', {}, 'Film o królu sawanny')
-        	)
-        )
-    );	
+						React.createElement('h2', {}, 'Król Lew'),
+						React.createElement('p', {}, 'Film o królu sawanny')
+					)
+				)
+		);	
 
 ReactDOM.render(element, document.getElementById('app'));
 */
 
 var movies = [
-  {
-  	id: 1,
-    title: 'Harry Potter',
-    desc: 'Film o czarodzieju',
-    img: './images/harry.jpg'
-  },
-  {
-  	id: 2,
-    title: 'Król Lew',
-    desc: 'Film o królu sawanny',
-    img: './images/lew.jpg'
-  },
-  {
-  	id: 3,
-  	title: 'Piraci z Karaibów',
-  	desc: 'Film o piratach',
-  	img: './images/piraci.jpeg'
-  },
-  {
-  	id: 4,
-  	title: 'Titanic',
-  	desc: 'Film o Titanicu',
-  	img: './images/titanic.jpg'
-  }  
+	{
+		id: 1,
+		title: 'Harry Potter',
+		desc: 'Film o czarodzieju',
+		img: './images/harry.jpg'
+	},
+	{
+		id: 2,
+		title: 'Król Lew',
+		desc: 'Film o królu sawanny',
+		img: './images/lew.jpg'
+	},
+	{
+		id: 3,
+		title: 'Piraci z Karaibów',
+		desc: 'Film o piratach',
+		img: './images/piraci.jpeg'
+	},
+	{
+		id: 4,
+		title: 'Titanic',
+		desc: 'Film o Titanicu',
+		img: './images/titanic.jpg'
+	}  
 ];
 
 var moviesElements = movies.map(function(movie) {
 	return React.createElement('li', {key: movie.id},
-		React.createElement('h2', {}, movie.title),
-		React.createElement('img', {src: movie.img}),
-		React.createElement('p', {}, movie.desc),
+		   React.createElement('h2', {}, movie.title),
+		   React.createElement('img', {src: movie.img}),
+		   React.createElement('p', {}, movie.desc)
 	);
 });
 
